39418b03fa54c3881003ddda37718435a50b6b25
Bessie Steinberg <bsteinberg@edx.org>
2019-05-20 16:16:08 -0400

    adding cover image

--- designer/apps/sitebranding/serializers.py
+++ designer/apps/sitebranding/serializers.py
@@ -1,16 +1,31 @@
 """ Theming / Site Branding Serializers"""
 from rest_framework import serializers
 from .models import SiteBranding
+from wagtail.wagtailimages.models import Image
+
+class ImageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Wagtail Images
+    """
+
+    class Meta(object):
+        model = Image
+        read_only = True
+        fields = (
+            'file'
+        )
 
 class SiteBrandingSerializer(serializers.ModelSerializer):
     """
     Serializer for the `SiteBranding` model.
     """
+    cover_image = ImageSerializer(many=False, read_only=True)
 
     class Meta(object):
         model = SiteBranding
         read_only = True
         fields = (
             'program_title',
+            'cover_image',
             'banner_border_color',
         )
