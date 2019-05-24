70a8e93936c40e4bfc7bd9e6cb61726823f5c3c0
Bessie Steinberg <bsteinberg@edx.org>
2019-05-24 18:07:34 -0400

    the base factories are working

--- designer/apps/sitebranding/tests/test_views.py
+++ designer/apps/sitebranding/tests/test_views.py
@@ -1,12 +1,15 @@
 """ Test Cases for Site Branding Views """
 from django.test import TestCase
+from .utils import SiteBrandingFactory
 
 
 class TestSiteBrandingViewSet(TestCase):
     """  Tests for SiteBrandingViewSet  """
 
     def setUp(self):
-
+        import pudb; pudb.set_trace()
+        self.site_branding = SiteBrandingFactory()
 
     def test_happy_path(self):
+        import pudb; pudb.set_trace()
         self.assertFalse(True)
