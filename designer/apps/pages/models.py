b0656da57b8229a6f99edb3a0823f9a324773843
Bessie Steinberg <bsteinberg@edx.org>
2019-06-10 17:13:36 -0400

    stuff

--- designer/apps/pages/models.py
+++ designer/apps/pages/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.api import APIField
 
 
 class IndexPage(Page):
@@ -13,6 +14,10 @@ class IndexPage(Page):
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
         InlinePanel('index_page_branding', label="Index Page Branding", max_num=1),
+    ]
+
+    api_fields = [
+        APIField('index_page_branding'),
     ]
 
 
