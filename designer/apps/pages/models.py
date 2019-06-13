12d88ade6371f8b91ee155ca68d8c498d28499c1
Bessie Steinberg <bsteinberg@edx.org>
2019-06-13 13:31:15 -0400

    mostly working

--- designer/apps/pages/models.py
+++ designer/apps/pages/models.py
@@ -6,6 +6,9 @@ from wagtail.wagtailcore.models import Page
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
 from wagtail.api import APIField
+from modelcluster.fields import ParentalKey
+from designer.apps.branding.models import Branding
+from django.db import models
 
 
 class IndexPage(Page):
@@ -13,12 +16,21 @@ class IndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
-        InlinePanel('index_page_branding', label="Index Page Branding", max_num=1),
+        InlinePanel('branding', label="Index Page Branding", max_num=1),
     ]
 
     api_fields = [
-        APIField('index_page_branding'),
+        APIField('branding'),
     ]
+
+
+# TODO: should this be with branding?
+class IndexPageBranding(Branding):
+    """
+    Branding specifically for the Index Page (The site level home page)
+    """
+    # TODO: site title
+    page = ParentalKey(IndexPage, on_delete=models.CASCADE, related_name='branding', unique=True)
 
 
 class ProgramPage(Page):
@@ -27,5 +39,18 @@ class ProgramPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
-        InlinePanel('program_page_branding', label="Program Page Branding", max_num=1),
+        InlinePanel('branding', label="Program Page Branding", max_num=1),
     ]
+
+    api_fields = [
+        APIField('branding'),
+    ]
+
+
+# TODO: squash migrations
+class ProgramPageBranding(Branding):
+    """
+    Branding specifically for the Program Page
+    """
+    # TODO: program title
+    page = ParentalKey(ProgramPage, on_delete=models.CASCADE, related_name='branding', unique=True)
