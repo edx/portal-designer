eae11cbd7cd43a29c12e7df4dd8f24f45fe14103
Christie Rice <8483753+crice100@users.noreply.github.com>
2019-08-09 14:58:24 -0400

    Switch check since is_staff is initially false upon user creation

--- designer/apps/core/handlers.py
+++ designer/apps/core/handlers.py
@@ -7,27 +7,23 @@ from django.dispatch import receiver
 
 from designer.apps.core.models import User
 
+OBSERVERS = 'Observers'
+
 
 @receiver(post_save, sender=User)
 def update_user_groups(sender, instance, created, **kwargs):  # pylint: disable=unused-argument
-    # TODO: logging, put handlers in submodule (?), create group and permissions via migration
+    # TODO: logging, put handlers in submodule (?), create group and permissions via migration, add doc, is there a
+    # better way to check "is created or just had is_staff set to true"? if we remove staff, do we need to remove
+    # from Observers?
     print('### in update_user_groups, username: ' + str(instance.username) + ', is_staff: ' + str(instance.is_staff))
-    if created:
-        print('### yep, user was just created')
-        if instance.is_staff:
-            print('### user is staff, will add to Observers group')
-            _add_user_to_observers(instance)
-        else:
-            print('### user is not staff, nothing to do')
+
+    if instance.is_staff and not instance.groups.filter(name=OBSERVERS).exists():
+        print('### user is staff but not in group, will add to Observers group')
+        observers = Group.objects.get(name=OBSERVERS)
+        if observers is None:
+            print('### oops, could not find group')
             return
+        instance.groups.add(observers)
+        instance.save()
     else:
-        print('### nope, user already existed')
-
-
-def _add_user_to_observers(user):
-    observers = Group.objects.get(name='Observers')
-    if observers is None:
-        print('### oops, could not find group')
-        return
-    user.groups.add(observers)
-    user.save()
+        print('### user is not staff or user was already in the group, nothing to do')
