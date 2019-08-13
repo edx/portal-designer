e1aee837814b52ebd2457dd660d1f9e26a0b69cf
Matt Tuchfarber <mtuchfarber@edx.org>
2019-08-13 10:24:07 -0400

    Disable the /admin view
    Everything can be done at /cms

--- designer/settings/base.py
+++ designer/settings/base.py
@@ -223,8 +223,7 @@ JWT_AUTH = {
 # Request the user's permissions in the ID token
 EXTRA_SCOPE = ['permissions']
 
-# TODO Set this to another (non-staff, ideally) path.
-LOGIN_REDIRECT_URL = '/admin/'
+LOGIN_REDIRECT_URL = '/cms/'
 # END AUTHENTICATION CONFIGURATION
 
 
