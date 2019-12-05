c534fbf467716904c91ac3f5f0bf38f34ac38c9a
Adam Stankiewicz <astankiewicz@edx.org>
2019-12-05 12:42:39 -0500

    Upgrade packages, specifically edx-drf-extensions (#64)

--- designer/apps/core/tests/test_views.py
+++ designer/apps/core/tests/test_views.py
@@ -39,7 +39,7 @@ class HealthTests(TestCase):
             }
         }
 
-        self.assertJSONEqual(response.content, expected_data)
+        self.assertJSONEqual(response.content.decode(), expected_data)
 
 
 class AutoAuthTests(TestCase):
