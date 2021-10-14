b5e4ff71dd1d4926d2fbacebba11b3180015caeb
Eric Herrera <eric.herrera@edunext.co>
2021-10-14 08:44:27 -0500

    chore: switch to drf-yasg
    Move from django-rest-swagger to drf-yasg and edx_api_doc_tools.

--- designer/urls.py
+++ designer/urls.py
@@ -21,7 +21,9 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import RedirectView
-from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from edx_api_doc_tools import make_api_info
+from rest_framework import permissions
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 from wagtail.core import urls as wagtail_urls
@@ -31,9 +33,17 @@ from designer.apps.core.wagtailadmin.views import SiteCreationView
 
 admin.autodiscover()
 
+
+api_info = make_api_info(title="designer API", version="v1")
+schema_view = get_schema_view(
+    api_info,
+    public=False,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = oauth2_urlpatterns + [
     url(r'^api/', include('designer.apps.api.urls')),
-    url(r'^api-docs/', get_swagger_view(title='designer API')),
+    url(r'^api-docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     # Use the same auth views for all logins, including those originating from the browseable API.
     url(r'^api-auth/', include(oauth2_urlpatterns)),
     url(r'^auto_auth/$', core_views.AutoAuth.as_view(), name='auto_auth'),
