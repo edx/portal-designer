8c77d2ed29f50d2dd65430582cdb6c274db9d449
zubairshakoorarbisoft <zubair.shakoor@arbisoft.com>
2023-09-11 23:12:32 +0500

    fix: django40 warnings rmeoved

--- designer/apps/api/urls.py
+++ designer/apps/api/urls.py
@@ -4,10 +4,10 @@ Root API URLs.
 All API URLs should be versioned, so urlpatterns should only
 contain namespaces for the active versions of the API.
 """
-from django.conf.urls import url, include
+from django.urls import re_path, include
 
 app_name = 'api'
 
 urlpatterns = [
-    url(r'^v1/', include(('designer.apps.api.v1.urls', 'v1'))),
+    re_path(r'^v1/', include(('designer.apps.api.v1.urls', 'v1'))),
 ]
